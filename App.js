71ab2a08dfb244c9537acba1e6bac34ef15b0e4a
rlatjdus0814 <kim01093314735@gmail.com>
2021-06-19 01:09:09 +0900

    StyleSheet property

--- App.js
+++ App.js
@@ -1,35 +1,51 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-//이렇게 상단에 가져와 사용할 이미지를 불러옵니다
-import favicon from "./assets/favicon.png"
 
 export default function App() {
   return (
     <View style={styles.container}>
-			{/*이미지 태그 soruce 부분에 가져온 미지 이름을 넣습니다 */}
-      <Image 
-        source={favicon}
-				// 사용설명서에 나와 있는 resizeMode 속성 값을 그대로 넣어 적용합니다
-        resizeMode={"repeat"}
-        style={styles.imageStyle}
-      />
+      <View style={styles.textContainer}>
+        <Text style={styles.textStyle}>스파르타 코딩클럽!!</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    //영역을 잡는 속성입니다. 따로 자세히 다룹니다.
+    //flex: 1은 전체 화면을 가져간다는 뜻입니다
     flex: 1,
+    //영역의 배경 색을 결정합니다
     backgroundColor: '#fff',
-    //혹시 미리 궁금하신 분들을 위해 언급하자면,
-    //justifyContent와 alignContent는 영역 안에 있는 콘텐츠들을 정렬합니다
+    //아래 두 속성은 영역 안의 컨텐츠들의 배치를 결정합니다. 
+    //flex를 자세히 다룰때 같이 자세히 다룹니다
     justifyContent:"center",
     alignContent:"center"
   },
-  imageStyle: {
-    width:"100%",
-    height:"100%",
-    alignItems:"center",
-    justifyContent:"center"
+  textContainer: {
+    //영역의 바깥 공간 이격을 뜻합니다(하단 이미지 참조)
+    margin:10,
+    //영역 안의 컨텐츠 이격 공간을 뜻합니다(하단 이미지 참조)
+    padding: 10,
+    //테두리의 구부러짐을 결정합니다. 지금 보면 조금 둥글죠?
+    borderRadius:10,
+    //테두리의 두께를 결정합니다
+    borderWidth:2,
+    //테두리 색을 결정합니다
+    borderColor:"#000",
+    //테구리 스타일을 결정합니다. 실선은 solid 입니다
+    borderStyle:"dotted",
+
+  },
+  textStyle: {
+    //글자 색을 결정합니다. rgb, 값 이름, 색상코드 모두 가능합니다
+    color:"red",
+    //글자의 크기를 결정합니다
+    fontSize:20,
+    //글자의 두께를 결정합니다
+    fontWeight:"700",
+    //가로기준으로 글자의 위치를 결정합니다
+    textAlign:"center"
   }
 });
