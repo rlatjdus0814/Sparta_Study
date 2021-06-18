16911e7139cad136cbfb1db9bf23acb5e2be1f47
rlatjdus0814 <kim01093314735@gmail.com>
2021-06-19 00:50:54 +0900

    first commit

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  console.disableYellowBox = true;
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
