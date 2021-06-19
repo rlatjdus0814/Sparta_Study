4104e89d8c213b86d341c2d15d5e918738afe784
rlatjdus0814 <kim01093314735@gmail.com>
2021-06-19 22:42:56 +0900

    honeytip mainPage

--- App.js
+++ App.js
@@ -1,51 +1,134 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import main from './assets/main.png';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView} from 'react-native';
 
-//flexDirection와 반대로 배치
 export default function App() {
+  console.disableYellowBox = true;
+  //return 구문 밖에서는 슬래시 두개 방식으로 주석
   return (
-    <View style={styles.container}>
-      <View style={styles.containerOne}>
-
-      </View>
-      <View style={styles.containerTwo}>
-        <View style={styles.innerOne}>
-         
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>나만의 꿀팁</Text>
+      <Image style={styles.mainImage} source={main} />
+      <ScrollView style={styles.middleContainer} horizontal={true}>
+        <TouchableOpacity style={styles.middleButton01}>
+          <Text style={styles.middleButtonText}>생활</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.middleButton02}>
+          <Text style={styles.middleButtonText}>재테크</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.middleButton03}>
+          <Text style={styles.middleButtonText}>반려견</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.middleButton04}>
+          <Text style={styles.middleButtonText}>꿀팁 찜</Text>
+        </TouchableOpacity>
+      </ScrollView>
+      <View style={styles.cardContainer}>
+        <View style={styles.card}>
+          <Image style={styles.cardImage} source={{uri:"https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Fpizza.png?alt=media&token=1a099927-d818-45d4-b48a-7906fd0d2ad3"}}/>
+          <View style={styles.cardText}>
+            <Text style={styles.cardTitle}>먹다 남은 피자를 촉촉하게!</Text>
+            <Text style={styles.cardDesc} numberOfLines={3}>먹다 남은 피자는 수분이 날라가기 때문에 처음처럼 맛있게 먹을 수 없는데요. 이럴 경우 그릇에 물을 받아 전자레인지 안에서 1분 30초에서 2분 정도 함께 돌려주면 촉촉하게 먹을 수 있습니다. 물이 전자레인지 안에서 수증기를 일으키고, 피자에 촉촉함을 더해줍니다.</Text>
+            <Text style={styles.cardDate}>2020.09.09</Text>
+          </View>
         </View>
-        <View style={styles.innerTwo}>
-          <View style={styles.content}></View>
-        </View>
-
       </View>
-    </View>
-  );
+    </ScrollView>
+  )
+
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex:1
+    backgroundColor: "#fff",
   },
-  containerOne: {
+  title: {
+    fontSize: 20,
+    fontWeight: "700",
+    marginTop: 50,
+    marginLeft: 20
+  },
+  mainImage: {
+    width: "90%",
+    height: 200,
+    borderRadius: 10,
+    marginTop: 20,
+    alignSelf: "center",
+  },
+  middleContainer: {
+    marginTop: 20,
+    marginLeft: 10,
+    height: 60,
+  },
+  middleButton01: {
+    width: 100,
+    height: 50,
+    padding: 15,
+    backgroundColor: "#fdc453",
+    borderRadius: 15,
+    margin: 7,
+  },
+  middleButton02: {
+    width: 100,
+    height: 50,
+    padding: 15,
+    backgroundColor: "#fe8d6f",
+    borderRadius: 15,
+    margin: 7,
+  },
+  middleButton03: {
+    width: 100,
+    height: 50,
+    padding: 15,
+    backgroundColor: "#9adbc5",
+    borderRadius: 15,
+    margin: 7,
+  },
+  middleButton04: {
+    width: 100,
+    height: 50,
+    padding: 15,
+    backgroundColor: "#f886a8",
+    borderRadius: 15,
+    margin: 7,
+  },
+  middleButtonText: {
+    color: "#fff",
+    fontWeight: "700",
+    textAlign: "center",
+  },
+  cardContainer: {
+    marginTop:10,
+    marginLeft:10
+  },
+  card:{
     flex:1,
-    backgroundColor:"red"
-  },
-  containerTwo:{
-    flex:2,
     flexDirection:"row",
-    backgroundColor:"yellow"
+    margin:10,
+    borderBottomWidth:0.5,
+    borderBottomColor:"#eee",
+    paddingBottom:10,
   },
-  innerOne: {
+  cardImage: {
     flex:1,
-    backgroundColor:"blue"
+    width:100,
+    height:100,
+    borderRadius:10,
   },
-  innerTwo: {
-    flex:4,
-    backgroundColor:"orange",
-    alignItems:"flex-end"
+  cardText: {
+    flex:2,
+    flexDirection:"column",
+    marginLeft:10,
   },
-  content: {
-    width:50,
-    height:50,
-    backgroundColor:"#000"
+  cardTitle: {
+    fontSize:20,
+    fontWeight:"700",
+  },
+  cardDesc: {
+    fontSize:15,
+  },
+  cardDate: {
+    fontSize:10,
+    color:"#A6A6A6",
   }
-});
+})
