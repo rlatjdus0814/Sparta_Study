745c8e0e5a5180ea67ddc57669e6f67b799a2161
rlatjdus0814 <kim01093314735@gmail.com>
2021-06-22 16:45:50 +0900

    state관리, Category 기능 추가

--- pages/MainPage.js
+++ pages/MainPage.js
@@ -8,16 +8,28 @@ import Loading from '../components/Loading';
 export default function App() {
   console.disableYellowBox = true;
   const [state,setState] = useState([])
+  const [cateState,setCateState] = useState([])
   const [ready,setReady] = useState(true)
 
   useEffect(()=>{
     setTimeout(()=>{
-        setState(data)
-        setReady(false)
+      let tip = data.tip;
+      setState(tip)
+      setCateState(tip)
+      setReady(false)
     },1000)
   },[])
 
-  let tip = data.tip;
+  const category = (cate) => {
+    if(cate == "전체보기"){
+        setCateState(state)
+    }else{
+      setCateState(state.filter((d)=>{
+        return d.category == cate
+      }))
+    }
+  }
+
   let todayWeather = 10 + 17;
   let todayCondition = "흐림"
 
@@ -27,14 +39,15 @@ export default function App() {
       <Text style={styles.weather}>오늘의 날씨: {todayWeather + '°C ' + todayCondition} </Text>
       <Image style={styles.mainImage} source={main}/>
       <ScrollView style={styles.middleContainer} horizontal indicatorStyle={"white"}>
-        <TouchableOpacity style={styles.middleButton01}><Text style={styles.middleButtonText}>생활</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.middleButton02}><Text style={styles.middleButtonText}>재테크</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.middleButton03}><Text style={styles.middleButtonText}>반려견</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.middleButton04}><Text style={styles.middleButtonText}>꿀팁 찜</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.middleButtonAll} onPress={()=>{category('전체보기')}}><Text style={styles.middleButtonTextAll}>전체보기</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.middleButton01} onPress={()=>{category('생활')}}><Text style={styles.middleButtonText}>생활</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.middleButton02} onPress={()=>{category('재테크')}}><Text style={styles.middleButtonText}>재테크</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.middleButton03} onPress={()=>{category('반려견')}}><Text style={styles.middleButtonText}>반려견</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.middleButton04} onPress={()=>{category('꿀팁 찜')}}><Text style={styles.middleButtonText}>꿀팁 찜</Text></TouchableOpacity>
       </ScrollView>
       <View style={styles.cardContainer}>
          { 
-          tip.map((content,i)=>{
+          cateState.map((content,i)=>{
             return ( <Card key={i} content={content} /> )
           })
          }
@@ -69,6 +82,15 @@ const styles = StyleSheet.create({
     marginLeft:10,
     height:60,
   },
+  middleButtonAll: {
+    width:100,
+    height:50,
+    padding:15,
+    backgroundColor:"#20b2aa",
+    borderColor:"deeppink",
+    borderRadius:15,
+    margin:7
+  },
   middleButton01: {
     width:100,
     height:50,
@@ -94,11 +116,6 @@ const styles = StyleSheet.create({
     borderRadius:15,
     margin:7,
   },
-  middleButtonText: {
-    color:"#fff",
-    fontWeight:"700",
-    textAlign:"center",
-  },
   middleButton04: {
     width:100,
     height:50,
@@ -107,38 +124,18 @@ const styles = StyleSheet.create({
     borderRadius:15,
     margin:7,
   },
+  middleButtonTextAll: {
+    color:"#fff",
+    fontWeight:"700",
+    textAlign:"center"
+  },
+  middleButtonText: {
+    color:"#fff",
+    fontWeight:"700",
+    textAlign:"center",
+  },
   cardContainer: {
     marginTop:10,
     marginLeft:10,
-  },
-  card:{
-    flex:1,
-    flexDirection:"row",
-    margin:10,
-    borderBottomWidth:0.5,
-    borderBottomColor:"#eee",
-    paddingBottom:10,
-  },
-  cardImage: {
-    flex:1,
-    width:100,
-    height:100,
-    borderRadius:10,
-  },
-  cardText: {
-    flex:2,
-    flexDirection:"column",
-    marginLeft:10,
-  },
-  cardTitle: {
-    fontSize:20,
-    fontWeight:"700"
-  },
-  cardDesc: {
-    fontSize:15,
-  },
-  cardDate: {
-    fontSize:10,
-    color:"#A6A6A6",
   },
 });
